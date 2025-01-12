4c04bef744963c3ec71e2e78698b05a4e6a1e117
musserdn <musserdn@gmail.com>
2025-01-11 18:09:05 -0600

    refactored and added the add role feature

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import logo from "asciiart-logo";
 import inquirer from "inquirer";
-import { viewAllEmployees, viewAllRoles, viewAllDepartments, addDepartment } from "./src/query.js";
+import { queryAllEmployees, queryAllRoles, queryAllDepartments, queryaddDepartment, queryaddRole } from "./src/query.js";
 import CliTable3 from "cli-table3";
 
 start();
@@ -47,97 +47,163 @@ function mainMenu() {
         .then((select) => {
             switch (select.mainMenu) {
                 case 'View All Employees':
-                    viewAllEmployees()
-                        .then((rows) => {
-                            const table = new CliTable3({
-                                head: ['ID', 'First Name', 'Last Name', 'Title', 'Department', 'Salary', 'Manager'],
-                                colWidths: [5, 20, 20, 30, 20, 20, 20],
-                                chars: { 'mid': '', 'left-mid': '', 'mid-mid': '', 'right-mid': '' },
-                            });
-                            rows.forEach(row => {
-                                table.push([
-                                    row.id,
-                                    row.first_name,
-                                    row.last_name,
-                                    row.title,
-                                    row.department,
-                                    row.salary,
-                                    row.manager
-                                ]);
-                            });
-                            console.log(table.toString());
-                            mainMenu();
-                        })
-                        .catch((error) => {
-                            console.error('Error in View All Employees case:', error);
-                            mainMenu();
-                        });
+                    viewAllEmployees();
                     break;
                 case 'View All Roles':
-                    viewAllRoles()
-                        .then((rows) => {
-                            const table = new CliTable3({
-                                head: ['ID', 'Title', 'Department', 'Salary'],
-                                colWidths: [5, 30, 20, 20],
-                                chars: { 'mid': '', 'left-mid': '', 'mid-mid': '', 'right-mid': '' },
-                            });
-                            rows.forEach(row => {
-                                table.push([
-                                    row.id,
-                                    row.title,
-                                    row.department,
-                                    row.salary
-                                ]);
-                            });
-                            console.log(table.toString());
-                            mainMenu();
-                        })
-                        .catch((error) => {
-                            console.error('Error in View All Roles case:', error);
-                            mainMenu();
-                        });
+                    viewAllRoles();
+                    break;
+                case 'Add Role':
+                    addRole();
                     break;
                 case 'View All Departments':
-                    viewAllDepartments()
-                        .then((rows) => {
-                            const table = new CliTable3({
-                                head: ['ID', 'Department'],
-                                colWidths: [5, 30],
-                                chars: { 'mid': '', 'left-mid': '', 'mid-mid': '', 'right-mid': '' },
-                            });
-                            rows.forEach(row => {
-                                table.push([
-                                    row.id,
-                                    row.department
-                                ]);
-                            });
-                            console.log(table.toString());
-                            mainMenu();
-                        })
-                        .catch((error) => {
-                            console.error('Error in View All Departments case:', error);
-                            mainMenu();
-                        });
+                    viewAllDepartments();
                     break;
                 case 'Add A Department':
-                    inquirer.prompt([
-                        {
-                            name: 'name',
-                            message: 'What is the name of the department?',
-                        },
-                    ]).then((newDepartment) => {
-                        addDepartment(newDepartment)
-                            .then(() => console.log('Department Added'))
-                            .then(() => mainMenu());
-                    })
-                        .catch((error) => {
-                            console.error('Error in View All Departments case:', error);
-                            mainMenu();
-                        });
+                    addDepartment();
                     break;
                 case 'Quit':
                     console.log('Amazing Bossmode, see you again soon!');
                     process.exit();
+                default:
+                    console.error('Invalid Selection, try again');
+                    mainMenu();
             }
         })
+}
+
+
+// Breaking out each case into a separate function
+function viewAllEmployees() {
+    queryAllEmployees()
+        .then((rows) => {
+            const table = new CliTable3({
+                head: ['ID', 'First Name', 'Last Name', 'Title', 'Department', 'Salary', 'Manager'],
+                colWidths: [5, 20, 20, 30, 20, 20, 20],
+                chars: { 'mid': '', 'left-mid': '', 'mid-mid': '', 'right-mid': '' },
+            });
+            rows.forEach(row => {
+                table.push([
+                    row.id,
+                    row.first_name,
+                    row.last_name,
+                    row.title,
+                    row.department,
+                    row.salary,
+                    row.manager
+                ]);
+            });
+            console.log(table.toString());
+            mainMenu();
+        })
+        .catch((error) => {
+            console.error('Error in View All Employees case:', error);
+            mainMenu();
+        });
+}
+
+function viewAllRoles() {
+    queryAllRoles()
+        .then((rows) => {
+            const table = new CliTable3({
+                head: ['ID', 'Title', 'Department', 'Salary'],
+                colWidths: [5, 30, 20, 20],
+                chars: { 'mid': '', 'left-mid': '', 'mid-mid': '', 'right-mid': '' },
+            });
+            rows.forEach(row => {
+                table.push([
+                    row.id,
+                    row.title,
+                    row.department,
+                    row.salary
+                ]);
+            });
+            console.log(table.toString());
+            mainMenu();
+        })
+        .catch((error) => {
+            console.error('Error in View All Roles case:', error);
+            mainMenu();
+        });
+}
+
+async function addRole() {
+    try {
+        const departments = await queryAllDepartments();
+
+
+        const departmentList = departments.map(dept => ({
+            name: dept.department,
+            value: dept.id
+        }));
+
+        const answers = await inquirer.prompt([
+            {
+                type: 'input',
+                name: 'title',
+                message: 'Enter the title of the new role:',
+                validate: input => input ? true : 'Title cannot be empty.'
+            },
+            {
+                type: 'number',
+                name: 'salary',
+                message: 'Enter the salary for the new role:',
+                validate: input => !isNaN(input) ? true : 'Please enter a valid number.'
+            },
+            {
+                type: 'list',
+                name: 'department_id',
+                message: 'Select the department for the new role:',
+                choices: departmentList
+            },
+        ]);
+
+        console.log(answers);
+        const newRole = await queryaddRole(answers);
+        console.log("New Role:", newRole);
+        mainMenu();
+
+    } catch (error) {
+        console.error('Error in Add Role prompt:', error);
+        mainMenu();
+    }
+}
+
+function viewAllDepartments() {
+    queryAllDepartments()
+        .then((rows) => {
+            const table = new CliTable3({
+                head: ['ID', 'Department'],
+                colWidths: [5, 30],
+                chars: { 'mid': '', 'left-mid': '', 'mid-mid': '', 'right-mid': '' },
+            });
+            rows.forEach(row => {
+                table.push([
+                    row.id,
+                    row.department
+                ]);
+            });
+            console.log(table.toString());
+            mainMenu();
+        })
+        .catch((error) => {
+            console.error('Error in View All Departments case:', error);
+            mainMenu();
+        });
+}
+
+function addDepartment() {
+    inquirer.prompt([
+        {
+            name: 'name',
+            message: 'What is the name of the department?',
+        },
+    ]).then((newDepartment) => {
+        queryaddDepartment(newDepartment)
+            .then(() => console.log('Department Added'))
+            .then(() => mainMenu());
+    })
+        .catch((error) => {
+            console.error('Error in View All Departments case:', error);
+            mainMenu();
+        });
 }
--- src/query.js
+++ src/query.js
@@ -2,7 +2,7 @@ import { pool, connectToDb } from './connection.js';
 
 await connectToDb();
 
-export async function viewAllEmployees() {
+export async function queryAllEmployees() {
     console.log('You selected View All Employees');
     try {
         const { rows } = await pool.query("SELECT e.id, e.first_name, e.last_name, r.title,d.name AS department, r.salary, CONCAT(m.first_name, ' ', m.last_name) AS manager FROM employee e INNER JOIN role r ON e.role_id = r.id INNER JOIN department d ON r.department_id = d.id LEFT JOIN employee m ON m.id = e.manager_id ORDER BY e.last_name;");
@@ -14,7 +14,7 @@ export async function viewAllEmployees() {
     }
 };
 
-export async function viewAllRoles() {
+export async function queryAllRoles() {
     console.log('You selected View All Roles');
     try {
         const { rows } = await pool.query("SELECT r.id,r.title,r.salary,d.name AS department FROM role r INNER JOIN department d ON r.department_id = d.id ORDER BY r.title;");
@@ -26,7 +26,7 @@ export async function viewAllRoles() {
     }
 };
 
-export async function viewAllDepartments() {
+export async function queryAllDepartments() {
     console.log('You selected View All Employees');
     try {
         const { rows } = await pool.query("SELECT id, name AS department FROM department ORDER BY department;");
@@ -38,7 +38,7 @@ export async function viewAllDepartments() {
     }
 };
 
-export async function addDepartment(newDepartment) {
+export async function queryaddDepartment(newDepartment) {
     console.log('You selected Add Department');
     try {
         const { rows } = await pool.query("INSERT INTO department (name) VALUES ($1)", [newDepartment.name]);
@@ -49,3 +49,21 @@ export async function addDepartment(newDepartment) {
         throw err;
     }
 };
+
+export async function queryaddRole(newRole) {
+    console.log('You selected Add Role');
+    try {
+        const query = `
+            INSERT INTO role (title, salary, department_id)
+            VALUES ($1, $2, $3)
+            RETURNING *;
+        `;
+        const values = [newRole.title, newRole.salary, newRole.department_id];
+        const { rows } = await pool.query(query, values);
+        console.log('addRole function executed successfully');
+        return rows[0]; // Return the inserted role
+    } catch (err) {
+        console.error('Error executing insert addRole:', err);
+        throw err;
+    }
+}
